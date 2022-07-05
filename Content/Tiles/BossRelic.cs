c00b8c1e6d67f70a4ea4dafe616298eb8a495987
Bogdanker <44428951+has2r@users.noreply.github.com>
2022-07-05 21:58:05 +0300

    че то там

--- Content/Tiles/BossRelic.cs
+++ Content/Tiles/BossRelic.cs
@@ -11,6 +11,7 @@ using Terraria.Localization;
 
 namespace Consolaria.Content.Tiles {
     public abstract class BossRelic : ModTile {
+
         public const int FrameWidth = 18 * 3;
         public const int FrameHeight = 18 * 4;
         public const int HorizontalFrames = 1;
@@ -37,49 +38,15 @@ namespace Consolaria.Content.Tiles {
             TileObjectData.newTile.CopyFrom(TileObjectData.Style3x4);
             TileObjectData.newTile.LavaDeath = false;
             TileObjectData.newTile.DrawYOffset = 2;
-            //TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft; 
             TileObjectData.newTile.StyleHorizontal = false;
-
-            /*TileObjectData.newTile.StyleWrapLimitVisualOverride = 2;
-            TileObjectData.newTile.StyleMultiplier = 2;
-            TileObjectData.newTile.StyleWrapLimit = 2;
-            TileObjectData.newTile.styleLineSkipVisualOverride = 0;*/
-
-            /*TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile); 
-            TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight;
-            TileObjectData.addAlternate(1);*/
 
             TileObjectData.addTile(Type);
 
             AddMapEntry(new Color(233, 207, 94), Language.GetText("MapObject.Relic"));
         }
 
-        /*public override void KillMultiTile(int i, int j, int frameX, int frameY) {
-            int placeStyle = frameX / FrameWidth;
-
-            int itemType = 0;
-            switch (placeStyle) {
-                case 0:
-                    itemType = ModContent.ItemType<Items.BossDrops.Lepus.LepusRelic>();
-                    break;
-                case 1:
-                    itemType = ModContent.ItemType<Items.BossDrops.Turkor.TurkorRelic>();
-                    break;
-                case 2:
-                    itemType = ModContent.ItemType<Items.BossDrops.Ocram.OcramRelic>();
-                    break;
-            }
-            if (itemType > 0)		
-                Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 32, itemType);	
-        }*/
-
         public override bool CreateDust(int i, int j, ref int type)
             => false;
-
-        /*public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY) {
-            tileFrameX %= FrameWidth; 
-            tileFrameY %= FrameHeight * 2; 
-        }*/
 
         public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref TileDrawInfo drawData) {
             if (drawData.tileFrameX % FrameWidth == 0 && drawData.tileFrameY % FrameHeight == 0)
@@ -113,6 +80,15 @@ namespace Consolaria.Content.Tiles {
             float offset = (float)Math.Sin(Main.GlobalTimeWrappedHourly * TwoPi / 5f);
             Vector2 drawPos = worldPos + offScreen - Main.screenPosition + new Vector2(0f, -40f) +
                               new Vector2(0f, offset * 4f);
+            spriteBatch.Draw(texture, drawPos, frame, color, 0f, origin, 1f, effects, 0f);
+
+			float scale = (float)Math.Sin(Main.GlobalTimeWrappedHourly * TwoPi / 2f) * 0.3f + 0.7f;
+			Color effectColor = color;
+			effectColor.A = 0;
+			effectColor = effectColor * 0.1f * scale;
+			for (float num5 = 0f; num5 < 1f; num5 += 355f / (678f * (float)Math.PI)) {
+				spriteBatch.Draw(texture, drawPos + (TwoPi * num5).ToRotationVector2() * (6f + offset * 2f), frame, effectColor, 0f, origin, 1f, effects, 0f);
+			}
         }
     }
 }

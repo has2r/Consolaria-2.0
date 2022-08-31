2780f7a3943be74ec308ad8f5797685bc815c5e6
peege-on <52748183+peege-on@users.noreply.github.com>
2022-08-31 19:45:34 +0100

    ass sorted fixes

--- Content/Projectiles/Friendly/Pets/Leprechaun.cs
+++ Content/Projectiles/Friendly/Pets/Leprechaun.cs
@@ -1,5 +1,7 @@
 ﻿using Consolaria.Common;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.Audio;
 using Terraria.ID;
@@ -8,6 +10,12 @@ using Terraria.ModLoader;
 namespace Consolaria.Content.Projectiles.Friendly.Pets {
     public class Leprechaun : ConsolariaPet {
         public override int maxFrames => 9;
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 12;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+        }
 
         public override void SetDefaults () {
             int width = 30; int height = 50;
@@ -53,6 +61,22 @@ namespace Consolaria.Content.Projectiles.Friendly.Pets {
                     Item.NewItem(Projectile.GetSource_FromAI(), Projectile.Center, ItemID.PlatinumCoin, 1);
                 else Item.NewItem(Projectile.GetSource_FromAI(), Projectile.Center, ItemID.CopperCoin, 1);
             }
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            Texture2D texture = (Texture2D)ModContent.Request<Texture2D>("Consolaria/Assets/Textures/Projectiles/Leprechaun_Rainbow");
+            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, texture.Height * 0.5f);
+            SpriteEffects effects = (Projectile.spriteDirection == -1) ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            for (int k = 0; k < Projectile.oldPos.Length - 1; k++)
+            {
+                Vector2 drawPos = Projectile.oldPos[k] + new Vector2(Projectile.width, Projectile.height) / 2f + Vector2.UnitY * Projectile.gfxOffY - Main.screenPosition;
+                Color color = new Color(150, 150, 150, 50);
+                float rotation = (float)Math.Atan2(Projectile.oldPos[k].Y - Projectile.oldPos[k + 1].Y, Projectile.oldPos[k].X - Projectile.oldPos[k + 1].X);
+                spriteBatch.Draw(texture, drawPos, null, color, rotation, drawOrigin, Projectile.scale - k / (float)Projectile.oldPos.Length + 0.5f, effects, 0f);
+            }
+            return false;
         }
     }
 }

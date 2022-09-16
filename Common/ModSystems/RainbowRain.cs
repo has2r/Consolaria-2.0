e5ee21f7ae22ed9f7ce9ef121acddf63221324cd
Bogdanker <44428951+has2r@users.noreply.github.com>
2022-09-16 23:57:38 +0300

    *

--- Common/ModSystems/RainbowRain.cs
+++ Common/ModSystems/RainbowRain.cs
@@ -7,7 +7,7 @@ namespace Consolaria.Common {
     public class RainbowRain : ModSystem {
         public override void PostUpdateWorld () {
             Vector2 randomPosition = new Vector2(Main.rand.Next(Main.maxTilesX * 16), 200);
-            if (Main.dayTime && (SeasonalEvents.configEnabled && SeasonalEvents.IsPatrickDay()) || (SeasonalEvents.configEnabled && Main.raining)) {
+            if (Main.dayTime && (SeasonalEvents.configEnabled && SeasonalEvents.IsPatrickDay()) || (!SeasonalEvents.configEnabled && Main.raining)) {
                 if (Main.time % 450 == 0 && Main.rand.NextBool(1 + RainbowDropDenominator())) {
                     int item = Item.NewItem(Entity.GetSource_NaturalSpawn(), randomPosition, 20, 20, ModContent.ItemType<Content.Items.Materials.RainbowPiece>(), 1, false, 0, false, false);
                     if (Main.netMode == NetmodeID.MultiplayerClient && item >= 0)

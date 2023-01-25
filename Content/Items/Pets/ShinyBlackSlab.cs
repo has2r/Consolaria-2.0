43d6501c9793f5d9a07e10f6584c59fd331a667f
KondiU <56745473+KondiU@users.noreply.github.com>
2023-01-25 17:15:53 +0100

    Added Android Pet & Ancient Plumber set
    -Added Android Pet (Currently unobtainable in normal gameplay but fully working)
    
    -Added Ancient Plumber set based on Old-gen variant of Plumber set (Currently unobtainable in normal gameplay but fully working, planning to make it obtainable through throwing regular Plumber set in Shimmer when tModLoader 1.4.4 will release)

---
+++ Content/Items/Pets/ShinyBlackSlab.cs
@@ -0,0 +1,47 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace Consolaria.Content.Items.Pets
+{
+	public class ShinyBlackSlab : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Shiny Black Slab");
+			Tooltip.SetDefault("Summons a pet android");
+		}
+
+		public override void SetDefaults() 
+		{
+			Item.useStyle = 1;
+			Item.useAnimation = 20;
+			Item.useTime = 20;
+			Item.width = 32;
+			Item.height = 32;
+			Item.useTime = 2;
+			Item.damage = 0;
+			Item.stack = 1;
+			Item.noMelee = true;
+			Item.rare = 3;
+			Item.value = Item.buyPrice(gold: 10);
+			Item.UseSound = SoundID.Item2;
+			Item.shoot = ModContent.ProjectileType<Content.Projectiles.Friendly.Pets.AndroidGuy>();
+			Item.buffType = ModContent.BuffType<Content.Buffs.Android>();
+		}
+
+		public override void UseStyle(Player player, Rectangle heldItemFrame) 
+		{
+			if (player.whoAmI == Main.myPlayer && player.itemTime == 0) 
+			{
+				player.AddBuff(Item.buffType, 3600);
+			}
+		}
+
+		public override void AddRecipes()
+		{
+
+		}
+	}
+}
---
+++ Content/Items/Vanity/AncientPlumbersHat.cs
@@ -0,0 +1,28 @@
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.Creative;
+
+namespace Consolaria.Content.Items.Vanity
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class AncientPlumbersHat : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			ArmorIDs.Head.Sets.DrawHatHair[Item.headSlot] = true;
+			DisplayName.SetDefault("Ancient Plumber's Hat");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() 
+		{
+			int width = 38; int height = 34;
+			Item.Size = new Vector2(width, height);
+
+			Item.value = Item.buyPrice(gold: 25);
+			Item.vanity = true;
+		}
+	}
+}

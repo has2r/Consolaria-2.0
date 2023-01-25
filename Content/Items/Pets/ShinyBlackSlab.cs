a409cd26be0f87be96c5800094e95980b56b929a
KondiU <56745473+KondiU@users.noreply.github.com>
2023-01-25 19:09:23 +0100

    Critical Shiny Black Slab animation fix

--- Content/Items/Pets/ShinyBlackSlab.cs
+++ Content/Items/Pets/ShinyBlackSlab.cs
@@ -1,47 +1,28 @@
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Microsoft.Xna.Framework;
 
-namespace Consolaria.Content.Items.Pets
+namespace Consolaria.Content.Items.Pets 
 {
-	public class ShinyBlackSlab : ModItem
+	public class ShinyBlackSlab : PetItem 
 	{
-		public override void SetStaticDefaults()
+		public override void SetStaticDefaults () 
 		{
 			DisplayName.SetDefault("Shiny Black Slab");
 			Tooltip.SetDefault("Summons a pet android");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId [Type] = 1;
 		}
 
-		public override void SetDefaults() 
-		{
-			Item.useStyle = 1;
-			Item.useAnimation = 20;
-			Item.useTime = 20;
-			Item.width = 32;
-			Item.height = 32;
-			Item.useTime = 2;
-			Item.damage = 0;
-			Item.stack = 1;
-			Item.noMelee = true;
+		public override void SetDefaults () {
+			Item.DefaultToVanitypet(ModContent.ProjectileType<Content.Projectiles.Friendly.Pets.AndroidGuy>(), ModContent.BuffType<Content.Buffs.Android>());
+
+			int width = 32; int height = 32;
+			Item.Size = new Vector2(width, height);
+
 			Item.rare = 5;
-			Item.value = Item.buyPrice(gold: 10);
-			Item.UseSound = SoundID.Item2;
-			Item.shoot = ModContent.ProjectileType<Content.Projectiles.Friendly.Pets.AndroidGuy>();
-			Item.buffType = ModContent.BuffType<Content.Buffs.Android>();
-		}
-
-		public override void UseStyle(Player player, Rectangle heldItemFrame) 
-		{
-			if (player.whoAmI == Main.myPlayer && player.itemTime == 0) 
-			{
-				player.AddBuff(Item.buffType, 3600);
-			}
-		}
-
-		public override void AddRecipes()
-		{
-
+			Item.value = Item.sellPrice(gold: 10);
 		}
 	}
 }

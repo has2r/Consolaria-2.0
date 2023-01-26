59165eea940ff7124395ad474343368dbd0c12ad
KondiU <kondi412@gmail.com>
2023-01-26 22:35:44 +0100

    Added Ancient Hero's Set
    -Added Ancient Hero's set based on Old-gen variant of Hero's set (Currently unobtainable in normal gameplay but fully working, planning to make it obtainable through throwing regular Hero's set in Shimmer when tModLoader 1.4.4 will release)

---
+++ Content/Items/Vanity/AncientHerosHat.cs
@@ -0,0 +1,27 @@
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.Creative;
+
+namespace Consolaria.Content.Items.Vanity
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class AncientHerosHat : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("Ancient Hero's Hat");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() 
+		{
+			int width = 38; int height = 34;
+			Item.Size = new Vector2(width, height);
+
+			Item.value = Item.sellPrice(silver: 20);
+			Item.vanity = true;
+		}
+	}
+}
---
+++ Content/Items/Vanity/AncientHerosShirt.cs
@@ -0,0 +1,27 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Consolaria.Content.Items.Vanity
+{
+	[AutoloadEquip(EquipType.Body)]
+	public class AncientHerosShirt : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("Ancient Hero's Shirt");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() 
+		{
+			int width = 30; int height = 18;
+			Item.Size = new Vector2(width, height);
+
+			Item.value = Item.sellPrice(silver: 10);
+			Item.vanity = true;
+		}
+	}
+}

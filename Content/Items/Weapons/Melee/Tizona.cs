4d0d399dc2dbd1e091bf69f811b0f2ae71643f95
Bogdanker <44428951+has2r@users.noreply.github.com>
2023-04-09 20:26:58 +0300

    *

--- Content/Items/Weapons/Melee/Tizona.cs
+++ Content/Items/Weapons/Melee/Tizona.cs
@@ -5,17 +5,16 @@ using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Consolaria.Content.Items.Weapons.Melee {
-    public class Tizona : ModItem {
-		public override void SetStaticDefaults () {
-			Item.ResearchUnlockCount = 1;
-		}
+	public class Tizona : ModItem {
+		public override void SetStaticDefaults ()
+			=> Item.ResearchUnlockCount = 1;
 
 		public override void SetDefaults () {
 			int width = 50; int height = width;
 			Item.Size = new Vector2(width, height);
 
 			Item.useStyle = ItemUseStyleID.Swing;
-			Item.useTime = Item.useAnimation = 18;
+			Item.useTime = Item.useAnimation = 24;
 
 			Item.noMelee = true;
 			Item.autoReuse = true;
@@ -34,9 +33,9 @@ namespace Consolaria.Content.Items.Weapons.Melee {
 			Item.shootSpeed = 12f;
 		}
 
-        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
-            Projectile.NewProjectile(player.GetSource_ItemUse(Item), position, new Vector2(player.direction, 0f), type, damage, knockback, player.whoAmI, player.direction * player.gravDir, player.itemAnimationMax);
-            return false;
-        }
-    }
+		public override bool Shoot (Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+			Projectile.NewProjectile(player.GetSource_ItemUse(Item), position, new Vector2(player.direction, 0f), type, damage, knockback, player.whoAmI, player.direction * player.gravDir, player.itemAnimationMax);
+			return false;
+		}
+	}
 }
--- Content/Projectiles/Friendly/Tizona.cs
+++ Content/Projectiles/Friendly/Tizona.cs
@@ -1,10 +1,14 @@
 ﻿using Consolaria.Common;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using System;
 using Terraria;
+using Terraria.GameContent;
+using Terraria.GameContent.Drawing;
 using Terraria.ID;
 using Terraria.ModLoader;
+using static Humanizer.In;
 
 namespace Consolaria.Content.Projectiles.Friendly {
     public class Tizona : ModProjectile {
@@ -37,8 +41,8 @@ namespace Consolaria.Content.Projectiles.Friendly {
             Player player = Main.player [Projectile.owner];
             float num = Projectile.localAI [0] / Projectile.ai [1];
             // can be changed for funny
-            float num2 = 0.5f;
-            float num3 = 1.2f;
+            float num2 = 0.8f;
+            float num3 = 1f;
 
             Projectile.Center = player.RotatedRelativePoint(player.MountedCenter) - Projectile.velocity;
             Projectile.scale = num3 + num * num2;
@@ -53,30 +57,50 @@ namespace Consolaria.Content.Projectiles.Friendly {
             }
 
             if (Main.rand.NextFloat() * 1.5f < Projectile.Opacity)
-                Dust.NewDustPerfect(position, 43, velocity * 1f, 100, Color.White * Projectile.Opacity, 1.2f * Projectile.Opacity);
+                Dust.NewDustPerfect(position, DustID.Shadowflame, velocity * 1f, 100, Color.White * Projectile.Opacity, 1.2f * Projectile.Opacity);
 
             if (Projectile.localAI [0] >= Projectile.ai [1])
                 Projectile.Kill();
         }
 
         public override void ModifyHitNPC (NPC target, ref NPC.HitModifiers modifiers) {
-            if (Main.netMode == NetmodeID.Server) {
-                return;
-            }
-            float angle = MathHelper.PiOver2;
-            Vector2 offset = new Vector2(0.2f);
-            Vector2 velocity = 1.5f * offset;
-            Vector2 position = Main.rand.NextVector2Circular(4f, 4f) * offset;
-            VisualEffect.Create<SlashHitEffect>(Mod.Assets.Request<Texture2D>("Assets/Textures/ClawsHitEffect").Value,
-                                                                        target.Center + target.velocity + position + Main.rand.NextVector2Circular(target.width / 3f, target.height / 3f),
-                                                                        angle.ToRotationVector2() * velocity * 0.5f,
-                                                                        Utils.MultiplyRGB(Lighting.GetColor(target.Center.ToTileCoordinates()) * 1.5f, Color.Lerp(Color1, Color2, Main.rand.NextFloat()) * 2.5f),
-                                                                        0f,
-                                                                        0.2f * 0.8f, 30);
+            ParticleOrchestraSettings particleOrchestraSettings;
+            Vector2 positionInWorld = Main.rand.NextVector2FromRectangle(target.Hitbox);
+            particleOrchestraSettings = default(ParticleOrchestraSettings);
+            particleOrchestraSettings.PositionInWorld = positionInWorld;
+            ParticleOrchestraSettings settings = particleOrchestraSettings;
+            ParticleOrchestrator.RequestParticleSpawn(clientOnly: false, ParticleOrchestraType.NightsEdge, settings, Projectile.owner);
         }
 
-        public override bool PreDraw (ref Color lightColor) {
-            SpriteBatch spriteBatch = Main.spriteBatch;
+        private void DrawLikeNightsEdge (SpriteBatch spriteBatch) {
+            Vector2 vector = Projectile.Center - Main.screenPosition;
+            Asset<Texture2D> asset = TextureAssets.Projectile [Projectile.type];
+            Rectangle rectangle = asset.Frame(1, 4);
+            Vector2 origin = rectangle.Size() / 2f;
+            float num = Projectile.scale * 1.1f;
+            SpriteEffects effects = (!(Projectile.ai [0] >= 0f)) ? SpriteEffects.FlipVertically : SpriteEffects.None;
+            float num2 = Projectile.localAI [0] / Projectile.ai [1];
+            float scale = Utils.Remap(num2, 0f, 0.6f, 0f, 1f) * Utils.Remap(num2, 0.6f, 1f, 1f, 0f);
+            float num3 = 0.975f;
+            float fromValue = Lighting.GetColor(Projectile.Center.ToTileCoordinates()).ToVector3().Length() / (float) Math.Sqrt(3.0);
+            fromValue = Utils.Remap(fromValue, 0.2f, 1f, 0f, 1f);
+            Color value = new Color(40, 20, 60);
+            spriteBatch.Draw(asset.Value, vector, rectangle, value * fromValue * scale, Projectile.rotation + Projectile.ai [0] * ((float) Math.PI / 4f) * -1f * (1f - num2), origin, num * num3, effects, 0f);
+            Color color = new Color(80, 40, 180);
+            Color value2 = Color.White * scale * 0.5f;
+            value2.A = (byte) ((float) (int) value2.A * (1f - fromValue));
+            Color value3 = value2 * fromValue * 0.5f;
+            value3.G = (byte) ((float) (int) value3.G * fromValue);
+            value3.R = (byte) ((float) (int) value3.R * (0.25f + fromValue * 0.75f));
+            spriteBatch.Draw(asset.Value, vector, rectangle, value3 * 0.15f, Projectile.rotation + Projectile.ai [0] * 0.01f, origin, num, effects, 0f);
+            spriteBatch.Draw(asset.Value, vector, rectangle, new Color(80, 30, 160) * fromValue * scale * 0.3f, Projectile.rotation, origin, num * 0.8f, effects, 0f);
+            spriteBatch.Draw(asset.Value, vector, rectangle, color * fromValue * scale * 0.7f, Projectile.rotation, origin, num * num3, effects, 0f);
+            spriteBatch.Draw(asset.Value, vector, asset.Frame(1, 4, 0, 3), Microsoft.Xna.Framework.Color.White * 0.3f * scale * (1f - fromValue * 0.7f), Projectile.rotation + Projectile.ai [0] * 0.01f, origin, num, effects, 0f);
+            Vector2 drawpos = vector + (Projectile.rotation + Utils.Remap(num2, 0f, 1f, 0f, (float) Math.PI / 2f) * Projectile.ai [0]).ToRotationVector2() * ((float) asset.Width() * 0.5f - 4f) * num;
+            DrawHelper.DrawPrettyStarSparkle(Projectile.Opacity, SpriteEffects.None, drawpos, new Color(255, 255, 255, 0) * scale * 0.5f, color, num2, 0f, 0.5f, 0.5f, 1f, (float) Math.PI / 4f, new Vector2(2f, 2f), Vector2.One);
+        }
+
+        private void DrawLikeExcalibur (SpriteBatch spriteBatch) {
             Vector2 vector = Projectile.Center - Main.screenPosition;
             Texture2D texture = (Texture2D) ModContent.Request<Texture2D>(Texture);
             Rectangle rectangle = texture.Frame(1, 4);
@@ -84,15 +108,15 @@ namespace Consolaria.Content.Projectiles.Friendly {
             float num = Projectile.scale * 1.1f;
             SpriteEffects effects = (!(Projectile.ai [0] >= 0f)) ? SpriteEffects.FlipVertically : SpriteEffects.None;
             float num2 = Projectile.localAI [0] / Projectile.ai [1];
-            float num3 = Utils.Remap(num2, 0f, 0.5f, 0f, 1.1f) * Utils.Remap(num2, 0.5f, 1f, 1.1f, 0f);
+            float num3 = Utils.Remap(num2, 0f, 0.5f, 0f, 1f) * Utils.Remap(num2, 0.5f, 1f, 1f, 0f);
             float num4 = 0.975f;
             float amount = num3;
             float fromValue = Lighting.GetColor(Projectile.Center.ToTileCoordinates()).ToVector3().Length() / (float) Math.Sqrt(3.0);
             fromValue = Utils.Remap(fromValue, 0.2f, 1f, 0f, 1f);
-            Color value = Color.Lerp(new Color(220, 20, 60), new Color(180, 30, 60), amount);
+            Color value = Color.Lerp(new Color(95, 70, 135), new Color(115, 80, 145), amount); //near sword
             spriteBatch.Draw(texture, vector, rectangle, value * fromValue * num3, Projectile.rotation + Projectile.ai [0] * ((float) Math.PI / 4f) * -1f * (1f - num2), origin, num, effects, 0f);
-            Color value2 = Color.Lerp(new Color(220, 160, 240), new Color(255, 60, 170), amount);
-            Color color = Color.Lerp(new Color(255, 255, 80), new Color(255, 60, 190), amount);
+            Color value2 = Color.Lerp(new Color(65, 60, 130), new Color(75, 70, 140), amount); //central part
+            Color color = Color.Lerp(new Color(130, 120, 220), new Color(100, 90, 200), amount); //main color
             Color value3 = Color.White * num3 * 0.5f;
             value3.A = (byte) (value3.A * (1f - fromValue));
             Color value4 = value3 * fromValue * 0.5f;
@@ -101,7 +125,7 @@ namespace Consolaria.Content.Projectiles.Friendly {
             spriteBatch.Draw(texture, vector, rectangle, value4 * 0.15f, Projectile.rotation + Projectile.ai [0] * 0.01f, origin, num, effects, 0f);
             spriteBatch.Draw(texture, vector, rectangle, color * fromValue * num3 * 0.3f, Projectile.rotation, origin, num, effects, 0f);
             spriteBatch.Draw(texture, vector, rectangle, value2 * fromValue * num3 * 0.5f, Projectile.rotation, origin, num * num4, effects, 0f);
-            spriteBatch.Draw(texture, vector, texture.Frame(1, 4, 0, 3), Color.White * 0.6f * num3, Projectile.rotation + Projectile.ai [0] * 0.01f, origin, num, effects, 0f);
+            spriteBatch.Draw(texture, vector, texture.Frame(1, 4, 0, 3), Color.Red * 0.6f * num3, Projectile.rotation + Projectile.ai [0] * 0.01f, origin, num, effects, 0f);
             spriteBatch.Draw(texture, vector, texture.Frame(1, 4, 0, 3), Color.White * 0.5f * num3, Projectile.rotation + Projectile.ai [0] * -0.05f, origin, num * 0.8f, effects, 0f);
             spriteBatch.Draw(texture, vector, texture.Frame(1, 4, 0, 3), Color.White * 0.4f * num3, Projectile.rotation + Projectile.ai [0] * -0.1f, origin, num * 0.6f, effects, 0f);
             float scaleFactor = num * 0.75f;
@@ -111,9 +135,12 @@ namespace Consolaria.Content.Projectiles.Friendly {
                 float scale = num5 / 12f;
                 DrawHelper.DrawPrettyStarSparkle(Projectile.Opacity, SpriteEffects.None, drawpos, new Color(255, 255, 255, 0) * num3 * scale, color, num2, 0f, 0.5f, 0.5f, 1f, num6, new Vector2(0f, Utils.Remap(num2, 0f, 1f, 3f, 0f)) * scaleFactor, Vector2.One * scaleFactor);
             }
-
             Vector2 drawpos2 = vector + (Projectile.rotation + Utils.Remap(num2, 0f, 0.6f, 0f, 0.9f) * Projectile.ai [0]).ToRotationVector2() * (texture.Width * 0.5f - 4f) * num;
-            DrawHelper.DrawPrettyStarSparkle(Projectile.Opacity, SpriteEffects.None, drawpos2, new Color(255, 255, 255, 0) * num3 * 0.5f, color, num2, 0f, 0.5f, 0.5f, 1f, 0f, new Vector2(2f, Utils.Remap(num2, 0f, 1f, 4f, 1f)) * scaleFactor, Vector2.One * scaleFactor);
+            DrawHelper.DrawPrettyStarSparkle(Projectile.Opacity, SpriteEffects.None, drawpos2, new Color(255, 0, 0) * num3 * 0.5f, color, num2, 0f, 0.5f, 0.5f, 1f, 0f, new Vector2(2f, Utils.Remap(num2, 0f, 1f, 4f, 1f)) * scaleFactor, Vector2.One * scaleFactor);
+        }
+        public override bool PreDraw (ref Color lightColor) {
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            DrawLikeExcalibur(spriteBatch);
             return false;
         }
     }

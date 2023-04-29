ea959f431c8fa5151bfdc9c285c5a3841067d204
KondiU <56745473+KondiU@users.noreply.github.com>
2023-04-29 18:58:35 +0200

    Update AncientPlumbersPants.cs

--- Content/Items/Vanity/AncientPlumbersPants.cs
+++ Content/Items/Vanity/AncientPlumbersPants.cs
@@ -1,30 +1,19 @@
 using Microsoft.Xna.Framework;
 using Terraria;
-<<<<<<< Updated upstream
-using Terraria.GameContent.Creative;
-using Terraria.ModLoader;
-
-namespace Consolaria.Content.Items.Vanity {
-=======
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Consolaria.Content.Items.Vanity
 {
->>>>>>> Stashed changes
     [AutoloadEquip(EquipType.Legs)]
 
 	public class AncientPlumbersPants : ModItem
 	{
 		public override void SetStaticDefaults() 
 		{
-<<<<<<< Updated upstream
-			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
-=======
 			DisplayName.SetDefault("Ancient Plumber's Pants");
 			Item.ResearchUnlockCount = 1;
 			ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.PlumbersPants;
->>>>>>> Stashed changes
 		}
 
 		public override void SetDefaults() 

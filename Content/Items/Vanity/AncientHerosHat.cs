cf8bdcb60929a5ac885faa5932b766eb4047d6f8
KondiU <kondi412@gmail.com>
2023-04-29 20:37:35 +0200

    Implemented back & fixed purple thread & it's recipe

--- Content/Items/Vanity/AncientHerosHat.cs
+++ Content/Items/Vanity/AncientHerosHat.cs
@@ -2,19 +2,31 @@ using Terraria.ModLoader;
 using Terraria;
 using Microsoft.Xna.Framework;
 
-namespace Consolaria.Content.Items.Vanity {
+namespace Consolaria.Content.Items.Vanity 
+{
 	[AutoloadEquip(EquipType.Head)]
 	public class AncientHerosHat : ModItem {
-		public override void SetStaticDefaults () {
+		public override void SetStaticDefaults () 
+		{
 			Item.ResearchUnlockCount = 1;
 		}
 
-		public override void SetDefaults () {
+		public override void SetDefaults () 
+		{
 			int width = 38; int height = 34;
 			Item.Size = new Vector2(width, height);
 
 			Item.value = Item.sellPrice(silver: 20);
 			Item.vanity = true;
+		}
+
+		public override void AddRecipes () 
+		{
+			CreateRecipe()
+				.AddIngredient(ItemID.Silk, 20)
+				.AddIngredient<PurpleThread>(3)
+				.AddTile(TileID.Loom)
+				.Register();
 		}
 	}
 }
--- Content/Items/Vanity/AncientHerosShirt.cs
+++ Content/Items/Vanity/AncientHerosShirt.cs
@@ -2,19 +2,32 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 
-namespace Consolaria.Content.Items.Vanity {
+namespace Consolaria.Content.Items.Vanity 
+{
 	[AutoloadEquip(EquipType.Body)]
-	public class AncientHerosShirt : ModItem {
-		public override void SetStaticDefaults () {
+	public class AncientHerosShirt : ModItem 
+	{
+		public override void SetStaticDefaults () 
+		{
 			Item.ResearchUnlockCount = 1;
 		}
 
-		public override void SetDefaults () {
+		public override void SetDefaults () 
+		{
 			int width = 30; int height = 18;
 			Item.Size = new Vector2(width, height);
 
 			Item.value = Item.sellPrice(silver: 10);
 			Item.vanity = true;
+		}
+
+		public override void AddRecipes () 
+		{
+			CreateRecipe()
+				.AddIngredient(ItemID.Silk, 20)
+				.AddIngredient<PurpleThread>(3)
+				.AddTile(TileID.Loom)
+				.Register();
 		}
 	}
 }

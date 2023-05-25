1480d90e3c64e95a9b4b112fe8cc12e659b58c4c
Bogdanker <44428951+has2r@users.noreply.github.com>
2023-05-25 21:33:05 +0300

    config update

--- Common/ConsolariaConfig.cs
+++ Common/ConsolariaConfig.cs
@@ -4,30 +4,22 @@ using Terraria.ModLoader.Config;
 namespace Consolaria.Common {
 	[Label("Configuration")]
 	public class ConsolariaConfig : ModConfig {
-		public override ConfigScope Mode => ConfigScope.ClientSide;
+		public override ConfigScope Mode => ConfigScope.ServerSide;
 
-		[Header("Seasonal Events")]
-		[Label("Restrict seasonal content by system date (Easter)")]
-		[Tooltip("If enabled the relevant items and enemies will only appear during Easter\nApril 1 - April 30")]
+		[Header("SeasonalEvents")]
 		[DefaultValue(false)]
 		[ReloadRequired]
 		public bool easterEnabled;
 
-		[Label("Restrict seasonal content by system date (Thanksgiving)")]
-		[Tooltip("If enabled the relevant items and enemies will only appear during Thanksgiving\nNovember 1 - November 30")]
 		[DefaultValue(false)]
 		[ReloadRequired]
 		public bool thanksgivingEnabled;
 
-		[Label("Restrict seasonal content by system date (Other events)")]
-		[Tooltip("If enabled the relevant items and enemies will only appear during the corresponding holidays or when Wishbone is used")]
 		[DefaultValue(false)]
 		[ReloadRequired]
 		public bool smallEventsEnabled;
 
 		[Header("Miscellaneous")]
-		[Label("Enable vanilla music for Consolaria bosses")]
-		[Tooltip("If enabled the custom soundtrack will be replaced by themes originally used in console versionsd")]
 		[DefaultValue(false)]
 		public bool vanillaBossMusic;
 	}

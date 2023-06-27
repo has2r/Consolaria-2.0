24a13d24ec544952afba2f548d878875b636f78b
Zerokk <mandrygin.2013@mail.ru>
2023-06-27 18:55:43 +0300

    moneypants npc

---
+++ Content/NPCs/Friendly/McMoneypants.cs
@@ -0,0 +1,283 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Bestiary;
+using Terraria.Chat;
+using Terraria.Localization;
+using Terraria.ModLoader.IO;
+
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.Xna.Framework;
+
+namespace Consolaria.Content.NPCs.Friendly;
+
+[AutoloadHead()]
+public class McMoneypants : ModNPC {
+    private static double _spawnTime = double.MaxValue;
+
+    public List<string> Names { get; private set; }
+        = new List<string>() { "Ryan Gosling",
+                               "Adolf Hitler",
+                               "Savelii Andrusovich" };
+
+    public List<string> Quotes { get; private set; } 
+        = new List<string>() { "I want to be cremated as it is my last hope for a smoking hot body.",
+                               "To the guy who invented zero, thanks for nothing.",
+                               "What was Forrest Gump’s email password? 1forrest1",
+                               "I was wondering why the ball was getting bigger. Then it hit me.",
+                               "Waking up this morning was an eye-opening experience.",
+                               "INVESTED!!!" };
+
+    internal static bool SpawnCondition
+        => Main.dayTime && Main.time >= _spawnTime && Main.time < 48600.0;
+
+    public const string BUTTON_TEXT = "Invest";
+
+    public override void SetStaticDefaults() {
+        int id = Type;
+
+        Main.npcFrameCount[id] = 25;
+
+        NPCID.Sets.ExtraFramesCount[id] = 9;
+
+        NPCID.Sets.DangerDetectRange[id] = 400;
+
+        NPCID.Sets.AttackFrameCount[id] = 4;
+        NPCID.Sets.AttackType[id] = 0;
+        NPCID.Sets.AttackTime[id] = 10;
+        NPCID.Sets.AttackAverageChance[id] = 10;
+
+        NPCID.Sets.HatOffsetY[id] = 0;
+    }
+
+    public override void SetDefaults() {
+        NPC.townNPC = true;
+        TownNPCStayingHomeless = true;
+
+        NPC.friendly = true;
+
+        NPC.width = 18;
+        NPC.height = 40;
+
+        NPC.aiStyle = 7;
+
+        NPC.damage = 10;
+        NPC.defense = 15;
+        NPC.lifeMax = 250;
+
+        NPC.HitSound = SoundID.NPCHit1;
+        NPC.DeathSound = SoundID.NPCDeath1;
+
+        NPC.knockBackResist = 0.5f;
+
+        AnimationType = NPCID.Guide;
+    }
+
+    public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        => bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] { 
+               BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Surface
+           });
+
+    public override bool PreAI()
+        => DespawnNPC();
+
+    private bool DespawnNPC() {
+        bool CheckConditions() {
+            return !Helper.IsNPCOnScreen(NPC.Center) && (!SpawnCondition || McMoneypantsWorldData.SomebodyInvested);
+        }
+
+        void NotifyDespawnInChat() {
+            if (Main.netMode == NetmodeID.SinglePlayer) {
+                Main.NewText(Language.GetTextValue("LegacyMisc.35", NPC.FullName), 50, 125, 255);
+            }
+            else {
+                ChatHelper.BroadcastChatMessage(NetworkText.FromKey("LegacyMisc.35", NPC.GetFullNetName()), new Color(50, 125, 255));
+            }
+        }
+
+        void KillNPC() {
+            Main.NewText(1);
+
+            NPC.active = false;
+            NPC.netSkip = -1;
+            NPC.life = 0;
+
+            ResetInvestedStatus();
+        }
+
+        if (CheckConditions())  {
+            NotifyDespawnInChat();
+            KillNPC();
+            return false;
+        }
+        return true;
+    }
+
+    public override void AI()
+        => NPC.homeless = true;
+
+    public override void HitEffect(NPC.HitInfo hit)
+        => OnHitDusts();
+
+    private void OnHitDusts() {
+        int dustAmount = NPC.life > 0 ? 1 : 5;
+        for (int k = 0; k < dustAmount; k++) {
+            Dust.NewDust(NPC.position, NPC.width, NPC.height, DustID.LifeDrain);
+        }
+    }
+
+    public override bool CanTownNPCSpawn(int numTownNPCs)
+        => false;
+
+    public override List<string> SetNPCNameList()
+        => Names;
+
+    public override string GetChat()
+        => Quotes[Main.rand.Next(Quotes.Count - 1)];
+
+    public override void SetChatButtons(ref string button, ref string button2) {
+        McMoneypantPlayerData modPlayer = Main.LocalPlayer.GetModPlayer<McMoneypantPlayerData>();
+        button = BUTTON_TEXT + (!modPlayer.PlayerInvested ? $" ({Helper.GetPriceText(modPlayer.PlayerInvestPrice)})" : string.Empty);
+    }
+
+    public override void OnChatButtonClicked(bool firstButton, ref string shopName)
+        => OnClick();
+
+    public override bool CanGoToStatue(bool toKingStatue) 
+        => !toKingStatue;
+
+    public override void TownNPCAttackStrength(ref int damage, ref float knockback) {
+        damage = 20;
+        knockback = 8f;
+    }
+
+    public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown) {
+        cooldown = 12;
+        randExtraCooldown = 20;
+    }
+
+    public override void TownNPCAttackProj(ref int projType, ref int attackDelay) {
+        projType = ProjectileID.GoldCoin;
+        attackDelay = 1;
+    }
+
+    public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset) {
+        multiplier = 6f;
+        randomOffset = 1.5f;
+    }
+
+    public static void SpawnNPCRandomly() {
+        int thisNPCType = ModContent.NPCType<McMoneypants>();
+        bool isMoneypantsThere = NPC.FindFirstNPC(thisNPCType) != -1;
+
+        void NotifySpawnInChat(NPC npc) {
+            if (Main.netMode == NetmodeID.SinglePlayer) {
+                Main.NewText(Language.GetTextValue("Announcement.HasArrived", npc.FullName), 50, 125, 255);
+            }
+            else {
+                ChatHelper.BroadcastChatMessage(NetworkText.FromKey("Announcement.HasArrived", npc.GetFullNetName()), new Color(50, 125, 255));
+            }
+        }
+
+        void UpdateSpawnTime() {
+            bool isMorning = Main.dayTime && Main.time == 0;
+            if (isMorning)  {
+                bool shouldComeWhen = Main.rand.NextChance(McMoneypantsWorldData.ChanceToSpawn) || McMoneypantsWorldData.SomebodyInvested;
+                if (!isMoneypantsThere && shouldComeWhen) {
+                    _spawnTime = Helper.GetRandomSpawnTime(5400, 8100);
+                }
+                else {
+                    _spawnTime = double.MaxValue;
+                }
+            }
+        }
+
+        void SpawnNPC() {
+            if (!isMoneypantsThere && Helper.CanTownNPCSpawn(SpawnCondition)) {
+                int npc = NPC.NewNPC(Terraria.Entity.GetSource_TownSpawn(), Main.spawnTileX * 16, Main.spawnTileY * 16, thisNPCType, 1);
+                NPC worldNPC = Main.npc[npc];
+                worldNPC.homeless = true;
+                worldNPC.direction = Main.spawnTileX >= WorldGen.bestX ? -1 : 1;
+                worldNPC.netUpdate = true;
+
+                _spawnTime = double.MaxValue;
+
+                NotifySpawnInChat(worldNPC);
+
+                ResetInvestedStatus();
+            }
+        }
+
+        UpdateSpawnTime();
+        SpawnNPC();
+    }
+
+    private void OnClick() {
+        McMoneypantPlayerData modPlayer = Main.LocalPlayer.GetModPlayer<McMoneypantPlayerData>();
+        if (modPlayer.PlayerInvested) {
+            return;
+        }
+
+        McMoneypantsWorldData.SomebodyInvested = true;
+
+        modPlayer.PlayerInvested = true;
+        modPlayer.PlayerInvestPrice += modPlayer.PlayerInvestPrice / 3;
+        modPlayer.PlayerInvestPrice += Item.buyPrice(gold: 5);
+
+        int lastElementIndex = Quotes.Count - 1;
+        Main.npcChatText = Quotes[lastElementIndex];
+    }
+
+    private static void ResetInvestedStatus() {
+        McMoneypantPlayerData modPlayer = Main.LocalPlayer.GetModPlayer<McMoneypantPlayerData>();
+        if (modPlayer.PlayerInvested) {
+            modPlayer.PlayerInvested = false;
+            return;
+        }
+
+        McMoneypantsWorldData.SomebodyInvested = false;
+    }
+}
+
+public class McMoneypantPlayerData : ModPlayer {
+    private static readonly int _startPrice = Item.buyPrice(gold: 15);
+
+    public int PlayerInvestPrice { get; internal set; } = _startPrice;
+
+    public bool PlayerInvested { get; internal set; } = false;
+
+    public override void SaveData(TagCompound tag) {
+        tag.Add("investPrice", PlayerInvestPrice);
+
+        tag.Add("invested", PlayerInvested);
+    }
+
+    public override void LoadData(TagCompound tag) {
+        PlayerInvestPrice = tag.GetInt("investPrice");
+
+        PlayerInvested = tag.GetBool("invested");
+    }
+}
+
+public class McMoneypantsWorldData : ModSystem {
+    public static bool SomebodyInvested { get; internal set; }
+
+    internal static double ChanceToSpawn { get; private set; } = 1f;
+
+    public override void SaveWorldData(TagCompound tag)
+        => tag.Add("isInvested", SomebodyInvested);
+
+    public override void LoadWorldData(TagCompound tag) 
+        => SomebodyInvested = tag.GetBool("isInvested");
+
+    public override void NetSend(BinaryWriter writer)
+        => writer.Write(SomebodyInvested);
+
+    public override void NetReceive(BinaryReader reader)
+        => SomebodyInvested = reader.ReadBoolean();
+
+    public override void PostUpdateTime()
+        => McMoneypants.SpawnNPCRandomly();
+}
--- Helper.cs
+++ Helper.cs
@@ -4,105 +4,176 @@ using System;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria.DataStructures;
 using Terraria.ID;
+using Terraria.Utilities;
 
-namespace Consolaria {
-    public static class Helper {
+namespace Consolaria; 
 
-        public static bool CanDrawArmorLayer (PlayerDrawSet drawInfo, int armorSlotID) {
-            if (drawInfo.drawPlayer.armor [armorSlotID].type == ItemID.None || drawInfo.drawPlayer.armor [armorSlotID] == null || drawInfo.drawPlayer.armor [armorSlotID] == new Item())
-                return true;
+public static class Helper {
+    public static bool NextChance(this UnifiedRandom rand, double chance)
+        => rand.NextDouble() <= chance;
+
+    public static string GetPriceText(int price) {
+        int investPrice = price;
+
+        string result = string.Empty;
+
+        int copperCoins = 0, silverCoins = 0, goldCoins = 0, platinumCoins = 0;
+
+        const int COPPER = 1000000, SILVER = 10000, GOLD = 100, PLATINUM = 1;
+
+        if (investPrice >= COPPER) {
+            copperCoins = investPrice / COPPER;
+            investPrice -= copperCoins * COPPER;
+        }
+        if (investPrice >= SILVER) {
+            silverCoins = investPrice / SILVER;
+            investPrice -= silverCoins * SILVER;
+        }
+        if (investPrice >= GOLD) {
+            goldCoins = investPrice / GOLD;
+            investPrice -= goldCoins * GOLD;
+        }
+        if (investPrice >= PLATINUM) {
+            platinumCoins = investPrice;
+        }
+
+        if (copperCoins > 0) {
+            string goldCoinText = Lang.inter[15].Value;
+            result += " " + copperCoins + " " + goldCoinText;
+        }
+        if (silverCoins > 0) {
+            string silverCoinText = Lang.inter[16].Value;
+            result += " " + silverCoins + " " + silverCoinText;
+        }
+        if (goldCoins > 0) {
+            string goldCoinText = Lang.inter[17].Value;
+            result += " " + goldCoins + " " + goldCoinText;
+        }
+        if (platinumCoins > 0) {
+            string platinumCoinText = Lang.inter[18].Value;
+            result += " " + platinumCoins + " " + platinumCoinText;
+        }
+
+        return result.Substring(1);
+    }
+
+    public static double GetRandomSpawnTime(double minTime, double maxTime)
+        => (maxTime - minTime) * Main.rand.NextDouble() + minTime;
+
+    public static bool CanTownNPCSpawn(bool spawnCondition) {
+        bool anyEvents = Main.IsFastForwardingTime() || Main.eclipse || Main.invasionType > 0 && Main.invasionDelay == 0 && Main.invasionSize > 0;
+        if (anyEvents) {
             return false;
         }
-
-        public static Color FadeToColor (Color first, Color second, float blendSpeed, int alpha) {
-            int r = (int) (second.R * blendSpeed + first.R * (1f - blendSpeed));
-            int g = (int) (second.G * blendSpeed + first.G * (1f - blendSpeed));
-            int b = (int) (second.B * blendSpeed + first.B * (1f - blendSpeed));
-            int a = alpha;
-            return new Color(r, g, b, a);
-        }
-
-        public static void BasicInWorldGlowmask (this Item item, SpriteBatch spriteBatch, Texture2D glowTexture, Color color, float rotation, float scale) {
-            spriteBatch.Draw(
-                glowTexture,
-                new Vector2(
-                    item.position.X - Main.screenPosition.X + item.width * 0.5f,
-                    item.position.Y - Main.screenPosition.Y + item.height - glowTexture.Height * 0.5f
-                ),
-                new Rectangle(0, 0, glowTexture.Width, glowTexture.Height),
-                color,
-                rotation,
-                glowTexture.Size() * 0.5f,
-                scale,
-                SpriteEffects.None,
-                0f);
-        }
-
-        public static Vector2 RandomPositon (Vector2 pos1, Vector2 pos2) {
-            Random _rand = new Random();
-            return new Vector2(_rand.Next((int) pos1.X, (int) pos2.X) + 1, _rand.Next((int) pos1.Y, (int) pos2.Y) + 1);
-        }
-
-        public static Vector2 VelocityFPTP (Vector2 pos1, Vector2 pos2, float speed) {
-            Vector2 move = pos2 - pos1;
-            return move * (speed / (float) Math.Sqrt(move.X * move.X + move.Y * move.Y));
-        }
-
-        public static float GradtoRad (float Grad) => Grad * (float) Math.PI / 180.0f;
-        public static float RadtoGrad (float Rad) => Rad * 180.0f / (float) Math.PI;
-
-        public static int GetNearestNPC (Vector2 Point, bool Friendly = false, bool NoBoss = false) {
-            float NearestNPCDist = -1;
-            int NearestNPC = -1;
-            foreach (NPC npc in Main.npc) {
-                if (!npc.active) continue;
-                if (NoBoss && npc.boss) continue;
-                if (!Friendly && (npc.friendly || npc.lifeMax <= 5)) continue;
-                if (NearestNPCDist == -1 || npc.Distance(Point) < NearestNPCDist) {
-                    NearestNPCDist = npc.Distance(Point);
-                    NearestNPC = npc.whoAmI;
-                }
-            }
-            return NearestNPC;
-        }
-
-        public static int GetNearestPlayer (Vector2 Point, bool Alive = false) {
-            float NearestPlayerDist = -1;
-            int NearestPlayer = -1;
-            foreach (Player player in Main.player) {
-                if (Alive && (!player.active || player.dead)) continue;
-                if (NearestPlayerDist == -1 || player.Distance(Point) < NearestPlayerDist) {
-                    NearestPlayerDist = player.Distance(Point);
-                    NearestPlayer = player.whoAmI;
-                }
-            }
-            return NearestPlayer;
-        }
-
-        public static Vector2 VelocityToPoint (Vector2 A, Vector2 B, float Speed) {
-            Vector2 Move = (B - A);
-            return Move * (Speed / (float) Math.Sqrt(Move.X * Move.X + Move.Y * Move.Y));
-        }
-
-        public static Vector2 RandomPointInArea (Vector2 A, Vector2 B)
-            => new Vector2(Main.rand.Next((int) A.X, (int) B.X) + 1, Main.rand.Next((int) A.Y, (int) B.Y) + 1);
-
-        public static Vector2 RandomPointInArea (Rectangle Area)
-            => new Vector2(Main.rand.Next(Area.X, Area.X + Area.Width), Main.rand.Next(Area.Y, Area.Y + Area.Height));
-
-        public static float RotateBetween2Points (Vector2 A, Vector2 B) => (float) Math.Atan2(A.Y - B.Y, A.X - B.X);
-
-        public static Vector2 CenterPoint (Vector2 A, Vector2 B) => new Vector2((A.X + B.X) / 2.0f, (A.Y + B.Y) / 2.0f);
-
-        public static Vector2 PolarPos (Vector2 Point, float Distance, float Angle, int XOffset = 0, int YOffset = 0) {
-            Vector2 ReturnedValue = new Vector2();
-            ReturnedValue.X = (Distance * (float) Math.Sin((double) Helper.RadtoGrad(Angle)) + Point.X) + XOffset;
-            ReturnedValue.Y = (Distance * (float) Math.Cos((double) Helper.RadtoGrad(Angle)) + Point.Y) + YOffset;
-            return ReturnedValue;
-        }
-
-        public static bool Chance (float chance) => Main.rand.NextFloat() <= chance;
-
-        public static Vector2 SmoothFromTo (Vector2 From, Vector2 To, float Smooth = 60f) => From + ((To - From) / Smooth);
+        return spawnCondition;
     }
+
+    public static bool IsNPCOnScreen(Vector2 center) {
+        int width = NPC.sWidth + NPC.safeRangeX * 2;
+        int height = NPC.sHeight + NPC.safeRangeY * 2;
+        Rectangle npcScreenRect = new((int)center.X - width / 2, (int)center.Y - height / 2, width, height);
+        foreach (Player player in Main.player) {
+            if (player.active && player.getRect().Intersects(npcScreenRect)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static bool CanDrawArmorLayer(PlayerDrawSet drawInfo, int armorSlotID) {
+        if (drawInfo.drawPlayer.armor [armorSlotID].type == ItemID.None || drawInfo.drawPlayer.armor [armorSlotID] == null || drawInfo.drawPlayer.armor [armorSlotID] == new Item())
+            return true;
+        return false;
+    }
+
+    public static Color FadeToColor(Color first, Color second, float blendSpeed, int alpha) {
+        int r = (int) (second.R * blendSpeed + first.R * (1f - blendSpeed));
+        int g = (int) (second.G * blendSpeed + first.G * (1f - blendSpeed));
+        int b = (int) (second.B * blendSpeed + first.B * (1f - blendSpeed));
+        int a = alpha;
+        return new Color(r, g, b, a);
+    }
+
+    public static void BasicInWorldGlowmask(this Item item, SpriteBatch spriteBatch, Texture2D glowTexture, Color color, float rotation, float scale) {
+        spriteBatch.Draw(
+            glowTexture,
+            new Vector2(
+                item.position.X - Main.screenPosition.X + item.width * 0.5f,
+                item.position.Y - Main.screenPosition.Y + item.height - glowTexture.Height * 0.5f
+            ),
+            new Rectangle(0, 0, glowTexture.Width, glowTexture.Height),
+            color,
+            rotation,
+            glowTexture.Size() * 0.5f,
+            scale,
+            SpriteEffects.None,
+            0f);
+    }
+
+    public static Vector2 RandomPositon(Vector2 pos1, Vector2 pos2) {
+        Random _rand = new Random();
+        return new Vector2(_rand.Next((int) pos1.X, (int) pos2.X) + 1, _rand.Next((int) pos1.Y, (int) pos2.Y) + 1);
+    }
+
+    public static Vector2 VelocityFPTP(Vector2 pos1, Vector2 pos2, float speed) {
+        Vector2 move = pos2 - pos1;
+        return move * (speed / (float) Math.Sqrt(move.X * move.X + move.Y * move.Y));
+    }
+
+    public static float GradtoRad(float Grad) => Grad * (float) Math.PI / 180.0f;
+    public static float RadtoGrad(float Rad) => Rad * 180.0f / (float) Math.PI;
+
+    public static int GetNearestNPC(Vector2 Point, bool Friendly = false, bool NoBoss = false) {
+        float NearestNPCDist = -1;
+        int NearestNPC = -1;
+        foreach (NPC npc in Main.npc) {
+            if (!npc.active) continue;
+            if (NoBoss && npc.boss) continue;
+            if (!Friendly && (npc.friendly || npc.lifeMax <= 5)) continue;
+            if (NearestNPCDist == -1 || npc.Distance(Point) < NearestNPCDist) {
+                NearestNPCDist = npc.Distance(Point);
+                NearestNPC = npc.whoAmI;
+            }
+        }
+        return NearestNPC;
+    }
+
+    public static int GetNearestPlayer(Vector2 Point, bool Alive = false) {
+        float NearestPlayerDist = -1;
+        int NearestPlayer = -1;
+        foreach (Player player in Main.player) {
+            if (Alive && (!player.active || player.dead)) continue;
+            if (NearestPlayerDist == -1 || player.Distance(Point) < NearestPlayerDist) {
+                NearestPlayerDist = player.Distance(Point);
+                NearestPlayer = player.whoAmI;
+            }
+        }
+        return NearestPlayer;
+    }
+
+    public static Vector2 VelocityToPoint(Vector2 A, Vector2 B, float Speed) {
+        Vector2 Move = (B - A);
+        return Move * (Speed / (float) Math.Sqrt(Move.X * Move.X + Move.Y * Move.Y));
+    }
+
+    public static Vector2 RandomPointInArea(Vector2 A, Vector2 B)
+        => new Vector2(Main.rand.Next((int) A.X, (int) B.X) + 1, Main.rand.Next((int) A.Y, (int) B.Y) + 1);
+
+    public static Vector2 RandomPointInArea(Rectangle Area)
+        => new Vector2(Main.rand.Next(Area.X, Area.X + Area.Width), Main.rand.Next(Area.Y, Area.Y + Area.Height));
+
+    public static float RotateBetween2Points(Vector2 A, Vector2 B) => (float) Math.Atan2(A.Y - B.Y, A.X - B.X);
+
+    public static Vector2 CenterPoint (Vector2 A, Vector2 B) => new Vector2((A.X + B.X) / 2.0f, (A.Y + B.Y) / 2.0f);
+
+    public static Vector2 PolarPos(Vector2 Point, float Distance, float Angle, int XOffset = 0, int YOffset = 0) {
+        Vector2 ReturnedValue = new();
+        ReturnedValue.X = (Distance * (float) Math.Sin((double) Helper.RadtoGrad(Angle)) + Point.X) + XOffset;
+        ReturnedValue.Y = (Distance * (float) Math.Cos((double) Helper.RadtoGrad(Angle)) + Point.Y) + YOffset;
+        return ReturnedValue;
+    }
+
+    public static bool Chance(float chance) => Main.rand.NextFloat() <= chance;
+
+    public static Vector2 SmoothFromTo(Vector2 From, Vector2 To, float Smooth = 60f) => From + ((To - From) / Smooth);
 }

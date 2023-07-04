3dea84b079f69cde5bac7bf100c73a41877013bb
Zerokk <mandrygin.2013@mail.ru>
2023-07-04 21:43:16 +0300

    tooltip

--- Content/Items/Consumables/CandiedFruit.cs
+++ Content/Items/Consumables/CandiedFruit.cs
@@ -4,6 +4,7 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.Enums;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace Consolaria.Content.Items.Consumables;
@@ -26,5 +27,11 @@ public class CandiedFruit : ModItem {
 	public override void SetDefaults() {
         Item.DefaultToFood(22, 22, 206, 3600 * 3);
         Item.SetShopValues(ItemRarityColor.Green2, Item.buyPrice(silver: 1));
+    }
+
+
+    public sealed override void ModifyTooltips(List<TooltipLine> tooltips) {
+        TooltipLine tooltip = new(Mod, "Food Tooltip", Language.GetTextValue("CommonItemTooltip.MediumStats"));
+        tooltips.Add(tooltip);
     }
 }

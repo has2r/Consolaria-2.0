f735f06a771f983dc2e675019b43c3f7a70abbd1
KondiU <kondi412@gmail.com>
2023-07-17 20:37:33 +0200

    Fixes
    - Fixed tile duplication glitch

--- Content/Tiles/LepusRelic.cs
+++ Content/Tiles/LepusRelic.cs
@@ -7,8 +7,5 @@ namespace Consolaria.Content.Tiles
 	public class LepusRelic : BossRelic
 	{
 		public override string RelicTextureName => "Consolaria/Content/Tiles/LepusRelic";
-
-		public override void KillMultiTile(int i, int j, int frameX, int frameY)
-			=> Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 32, ModContent.ItemType<Items.BossDrops.Lepus.LepusRelic>());	
 	}
 }

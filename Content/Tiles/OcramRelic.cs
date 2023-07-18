6e163c84cb1d09e001bf99ae6c7800b130e5cf73
KondiU <56745473+KondiU@users.noreply.github.com>
2023-07-18 18:19:45 +0200

    Fixes (#22)
    - Fixed tile duplication glitch

--- Content/Tiles/OcramRelic.cs
+++ Content/Tiles/OcramRelic.cs
@@ -7,8 +7,5 @@ namespace Consolaria.Content.Tiles
 	public class OcramRelic : BossRelic
 	{
 		public override string RelicTextureName => "Consolaria/Content/Tiles/OcramRelic";
-
-		public override void KillMultiTile(int i, int j, int frameX, int frameY)
-			=> Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 32, ModContent.ItemType<Items.BossDrops.Ocram.OcramRelic>());	
 	}
 }
--- Content/Tiles/TurkorTrophy.cs
+++ Content/Tiles/TurkorTrophy.cs
@@ -19,8 +19,5 @@ namespace Consolaria.Content.Tiles {
             AddMapEntry(new Color(120, 85, 60), Language.GetText("MapObject.Trophy"));
             DustType = 7;
         }
-
-        public override void KillMultiTile (int i, int j, int frameX, int frameY)
-            => Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 48, 48, ModContent.ItemType<Items.BossDrops.Turkor.TurkorTrophy>());
     }
 }

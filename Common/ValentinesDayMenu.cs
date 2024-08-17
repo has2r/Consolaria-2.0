80a243f7f3dbe2724398a12d4af2eebd66c6e03b
KondiU <kondi412@gmail.com>
2024-08-17 23:04:08 +0200

    Added Valentine's Day Logo

---
+++ Common/ValentinesDayMenu.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Consolaria.Common {
+    internal class ValentinesDayMenu : ModMenu {
+        public override string DisplayName => "Terraria (Valentine's Day)";
+
+        public override Asset<Texture2D> Logo => ModContent.Request<Texture2D>("Consolaria/Assets/Textures/ValentinesDayLogo");
+
+        public override bool PreDrawLogo (SpriteBatch spriteBatch, ref Vector2 logoDrawCenter, ref float logoRotation, ref float logoScale, ref Color drawColor) {
+            logoDrawCenter += new Vector2(0, 14);
+            logoScale *= 1f;
+            return true;
+        }
+    }
+}

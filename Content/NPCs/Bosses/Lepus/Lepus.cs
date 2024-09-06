26512967c78dc229fabf096b7b2543fe437ca17a
kittenchilly <mario.pikmin.zelda@gmail.com>
2024-09-06 02:50:55 -0500

    Fix Boss HP scaling in multiplayer (#31)

--- Content/NPCs/Bosses/Lepus/Lepus.cs
+++ Content/NPCs/Bosses/Lepus/Lepus.cs
@@ -179,14 +179,8 @@ namespace Consolaria.Content.NPCs.Bosses.Lepus {
         }
 
         public override void ApplyDifficultyAndPlayerScaling (int numPlayers, float balance, float bossAdjustment) {
-            NPC.lifeMax = (int) (NPC.lifeMax * 0.5f * 1.25f);
-            NPC.damage = (int) (NPC.damage * 0.65f);
-            if (numPlayers <= 1) return;
-            float healthBoost = 0.35f;
-            for (int k = 1; k < numPlayers; k++) {
-                NPC.lifeMax += (int) (NPC.lifeMax * healthBoost);
-                healthBoost += (1 - healthBoost) / 3;
-            }
+            NPC.lifeMax = (int)((double)NPC.lifeMax * 0.5f * (double)balance * (double)bossAdjustment);
+            NPC.damage = (int)((double)NPC.damage * 0.65f);
         }
 
         public override void SetBestiary (BestiaryDatabase database, BestiaryEntry bestiaryEntry)
--- Content/NPCs/Bosses/Turkor/TurkortheUngrateful.cs
+++ Content/NPCs/Bosses/Turkor/TurkortheUngrateful.cs
@@ -80,15 +80,9 @@ namespace Consolaria.Content.NPCs.Bosses.Turkor
 			if (!Main.dedServ) Music = ModContent.GetInstance<ConsolariaConfig>().vanillaBossMusicEnabled ? MusicID.Boss1 : MusicLoader.GetMusicSlot(Mod, "Assets/Music/Turkor");
 		}
 
-		public override void ApplyDifficultyAndPlayerScaling (int numPlayers, float balance, float bossAdjustment)/* tModPorter Note: bossLifeScale -> balance (bossAdjustment is different, see the docs for details) */ {
-			NPC.lifeMax = (int) (NPC.lifeMax * 0.5f * 1.4f);
-			NPC.damage = (int) (NPC.damage * 0.65f);
-			if (numPlayers <= 1) return;
-			float healthBoost = 0.35f;
-			for (int k = 1; k < numPlayers; k++) {
-				NPC.lifeMax += (int) (NPC.lifeMax * healthBoost);
-				healthBoost += (1 - healthBoost) / 3;
-			}
+		public override void ApplyDifficultyAndPlayerScaling (int numPlayers, float balance, float bossAdjustment) {
+            NPC.lifeMax = (int)((double)NPC.lifeMax * 0.5f * (double)balance * (double)bossAdjustment);
+            NPC.damage = (int)((double)NPC.damage * 0.65f);
 		}
 
 		public override void SetBestiary (BestiaryDatabase database, BestiaryEntry bestiaryEntry) {

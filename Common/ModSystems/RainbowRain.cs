73a2b4319fe0c2f62d290349737f446ecf9906e8
Serork <mandrygin.2013@mail.ru>
2025-03-21 17:41:05 +0400

    Rainbow drop rate reduced by 30%

--- Common/ModSystems/RainbowRain.cs
+++ Common/ModSystems/RainbowRain.cs
@@ -8,7 +8,7 @@ namespace Consolaria.Common {
         public override void PostUpdateWorld () {
             Vector2 randomPosition = new Vector2(Main.rand.Next(Main.maxTilesX * 16), 200);
             if (Main.dayTime && (SeasonalEvents.configEnabled && SeasonalEvents.IsPatrickDay()) || (!SeasonalEvents.configEnabled && Main.raining)) {
-                if (Main.time % 450 == 0 && Main.rand.NextBool(1 + RainbowDropDenominator())) {
+                if (Main.time % 450 == 0 && Main.rand.NextBool(1 + RainbowDropDenominator()) && Main.rand.Next(10) < 7) {
                     int item = Item.NewItem(Entity.GetSource_NaturalSpawn(), randomPosition, 20, 20, ModContent.ItemType<Content.Items.Materials.RainbowPiece>(), 1, false, 0, false, false);
                     if (Main.netMode == NetmodeID.MultiplayerClient && item >= 0)
                         NetMessage.SendData(MessageID.SyncItem, -1, -1, null, item, 1f, 0f, 0f, 0, 0, 0);

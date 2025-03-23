37cb024e79ccbb41edf869d917decde0495c170f
peege-on <52748183+peege-on@users.noreply.github.com>
2025-03-23 14:02:57 +0000

    Fixes and descriptions

--- Common/ModSystems/RainbowRain.cs
+++ Common/ModSystems/RainbowRain.cs
@@ -9,7 +9,7 @@ namespace Consolaria.Common {
         public override void PostUpdateWorld() {
             Vector2 randomPosition = new Vector2(Main.rand.Next(Main.maxTilesX * 16), 200);
             if (Main.dayTime && (SeasonalEvents.configEnabled && SeasonalEvents.IsPatrickDay()) || (!SeasonalEvents.configEnabled && Main.raining)) {
-                if (Main.time % 450 == 0 && Main.rand.NextBool(1 + RainbowDropDenominator()) && Main.rand.Next(10) < 7) {
+                if (Main.time % 450 == 0 && Main.rand.NextBool(1 + RainbowDropDenominator())) {
                     int item = Item.NewItem(Entity.GetSource_NaturalSpawn(), randomPosition, 20, 20, ModContent.ItemType<Content.Items.Materials.RainbowPiece>(), 1, false, 0, false, false);
                     if (Main.netMode == NetmodeID.MultiplayerClient && item >= 0)
                         NetMessage.SendData(MessageID.SyncItem, -1, -1, null, item, 1f, 0f, 0f, 0, 0, 0);
@@ -19,8 +19,8 @@ namespace Consolaria.Common {
 
         private static int RainbowDropDenominator() {
             if (SeasonalEvents.configEnabled)
-                return SeasonalEvents.IsPatrickDay() ? 3 : 0;
-            else return Main.raining ? 5 : 0;
+                return SeasonalEvents.IsPatrickDay() ? 6 : 0;
+            else return Main.raining ? 10 : 0;
         }
     }
 }

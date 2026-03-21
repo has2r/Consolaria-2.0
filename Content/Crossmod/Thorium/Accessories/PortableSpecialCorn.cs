5022feb4d4444f0f2e565f6a39c072465d9f14d8
Serork <mandrygin.2013@mail.ru>
2026-03-22 01:42:36 +0400

    Portable Special Corn

---
+++ Content/Crossmod/Thorium/Accessories/PortableSpecialCorn.cs
@@ -0,0 +1,13 @@
+﻿using Terraria;
+
+namespace Consolaria.Content.Crossmod.Thorium.Accessories;
+
+public sealed class PortableSpecialCorn : ThoriumItem_BardBase {
+    public override void SetBardDefaults() {
+        Item.DefaultToAccessory();
+
+        Item.SetSizeValues(36, 34);
+
+        Item.SetShopValues(Terraria.Enums.ItemRarityColor.White0, Item.sellPrice());
+    }
+}

b66b979695b27c29fe3f1c13590a90e229bd3ebf
Serork <mandrygin.2013@mail.ru>
2026-04-01 21:20:04 +0400

    Update ValentineRing.cs

--- Content/Items/Accessories/ValentineRing.cs
+++ Content/Items/Accessories/ValentineRing.cs
@@ -1,9 +1,17 @@
-using Microsoft.Xna.Framework;
+using Consolaria.Common;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using ReLogic.Content;
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 
 using Terraria;
+using Terraria.Chat;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -13,12 +21,14 @@ namespace Consolaria.Content.Items.Accessories {
     [AutoloadEquip(EquipType.HandsOn)]
     public class ValentineRing : ModItem {
         private static string SAVEKEY => nameof(Consolaria) + "valentinering";
+        private static string SPECIALKEY1 => nameof(ValentineRing) + "isInOwnerInventory2";
+        private static string SPECIALKEY2 => nameof(ValentineRing) + "isNotInOwnerInventory2";
 
         private string _ownerName = string.Empty;
 
         public bool HasOwner => !_ownerName.Equals(string.Empty);
 
-        public bool IsInOwnerInventory(Player owner) => _ownerName.Equals(owner.name);
+        public bool IsInOwnerInventory(Player owner) => !_ownerName.Equals(owner.name);
 
         public override void SetStaticDefaults() {
             Item.ResearchUnlockCount = 1;
@@ -30,15 +40,57 @@ namespace Consolaria.Content.Items.Accessories {
             }
 
             bool isInOwnerInventory = IsInOwnerInventory(Main.LocalPlayer);
+            Color specialTooltipColor_Red = new(205, 75, 115);
+            Color specialTooltipColor_Gray = new(130, 130, 130);
 
             if (isInOwnerInventory) {
                 tooltips.Add(new TooltipLine(Mod, nameof(ValentineRing) + "isInOwnerInventory", Language.GetTextValue("Mods.Consolaria.ValentineRingTooltip1")));
-                tooltips.Add(new TooltipLine(Mod, nameof(ValentineRing) + "isInOwnerInventory2", Language.GetText("Mods.Consolaria.ValentineRingTooltip3").Format(_ownerName)));
+                TooltipLine specialTooltip = new(Mod, SPECIALKEY1, Language.GetText("Mods.Consolaria.ValentineRingTooltip3").Format(_ownerName));
+                specialTooltip.OverrideColor = specialTooltipColor_Gray;
+                tooltips.Add(specialTooltip);
                 return;
             }
+            {
+                tooltips.Add(new TooltipLine(Mod, nameof(ValentineRing) + "isNotInOwnerInventory", Language.GetTextValue("Mods.Consolaria.ValentineRingTooltip2")));
+                TooltipLine specialTooltip = new(Mod, SPECIALKEY2, Language.GetText("Mods.Consolaria.ValentineRingTooltip4").Format(_ownerName));
+                specialTooltip.OverrideColor = specialTooltipColor_Red;
+                tooltips.Add(specialTooltip);
+            }
+        }
 
-            tooltips.Add(new TooltipLine(Mod, nameof(ValentineRing) + "isNotInOwnerInventory", Language.GetTextValue("Mods.Consolaria.ValentineRingTooltip2")));
-            tooltips.Add(new TooltipLine(Mod, nameof(ValentineRing) + "isNotInOwnerInventory2", Language.GetText("Mods.Consolaria.ValentineRingTooltip4").Format(_ownerName)));
+        public override bool PreDrawTooltipLine(DrawableTooltipLine line, ref int yOffset) {
+            if (line.Name.Equals(SPECIALKEY2)) {
+                Texture2D icon = TextureAssets.Item[ItemID.Heart].Value;
+                Vector2 getPosition(float waveOffset = 0f) => new Vector2(line.X, line.Y) + new Vector2(10, 10 + (float)Math.Sin(Main.timeForVisualEffects / 20f + waveOffset) * 2f);
+                float scale = Math.Max(icon.Width, icon.Height) / 20f;
+                Color color = Color.Lerp(line.OverrideColor.Value, Color.Lerp(line.OverrideColor.Value, GetShimmerGradient().MultiplyAlpha(1f), 0.25f), 1f);
+                color = Color.Lerp(color, Color.Red, 0.25f);
+                SpriteBatch batch = Main.spriteBatch;
+                float maxRotation = 0.25f;
+                float rotationWaveFrequency = 2.5f;
+                for (float num5 = 0f; num5 < 1f; num5 += 0.25f) {
+                    Vector2 vector2 = (num5 * ((float)Math.PI * 2f)).ToRotationVector2() * 2f * scale;
+                    batch.Draw(icon, getPosition(0f) + vector2, null, color.MultiplyAlpha(0f) * 0.125f, Helper.Wave(-maxRotation, maxRotation, rotationWaveFrequency, 0f), icon.Size() / 2, scale, default, 0);
+                }
+                batch.Draw(icon, getPosition(0f), null, color.MultiplyAlpha(0f) * 1f, Helper.Wave(-maxRotation, maxRotation, rotationWaveFrequency, 0f), icon.Size() / 2, scale, default, 0);
+                Utils.DrawBorderString(Main.spriteBatch, line.Text, new Vector2(line.X + 20f, line.Y), color.MultiplyAlpha(0.5f));
+                Vector2 offset = Vector2.UnitX * FontAssets.MouseText.Value.MeasureString(line.Text).X + Vector2.UnitX * 20f;
+                for (float num5 = 0f; num5 < 1f; num5 += 0.25f) {
+                    Vector2 vector2 = (num5 * ((float)Math.PI * 2f)).ToRotationVector2() * 2f * scale;
+                    batch.Draw(icon, getPosition(2f) + offset + vector2, null, color.MultiplyAlpha(0f) * 0.125f, Helper.Wave(-maxRotation, maxRotation, rotationWaveFrequency, 2f), icon.Size() / 2, scale, default, 0);
+                }
+                batch.Draw(icon, getPosition(2f) + offset, null, color.MultiplyAlpha(0f), Helper.Wave(-maxRotation, maxRotation, rotationWaveFrequency, 2f), icon.Size() / 2, scale, default, 0);
+
+                return false;
+            }
+
+            return base.PreDrawTooltipLine(line, ref yOffset);
+        }
+
+        public static Color GetShimmerGradient() {
+            float factor = MathF.Sin((float)Main.timeForVisualEffects * 0.1f) * 0.5f + 0.5f;
+            Color color = Color.Lerp(Color.Lerp(Color.White, new Color(150, 214, 245), factor), Color.Lerp(new Color(150, 214, 245), new Color(240, 146, 251), factor), factor);
+            return color;
         }
 
         public override void SetDefaults() {

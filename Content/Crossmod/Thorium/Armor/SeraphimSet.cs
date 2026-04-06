9485d9b4c1a312b8603a7a40a2497b0642ebb23f
Serork <mandrygin.2013@mail.ru>
2026-04-06 22:51:23 +0400

    adjs

--- Content/Crossmod/Thorium/Armor/SeraphimSet.cs
+++ Content/Crossmod/Thorium/Armor/SeraphimSet.cs
@@ -1,24 +1,39 @@
 ﻿using Terraria;
 using Terraria.Enums;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 using ThoriumMod;
+using ThoriumMod.Utilities;
 
 namespace Consolaria.Content.Crossmod.Thorium.Armor;
 
 [AutoloadEquip(EquipType.Head)]
 public sealed class SeraphimHelmet : ThoriumItem_HealerBase {
+    public override void SetStaticDefaults() {
+        Item.ResearchUnlockCount = 1;
+        ItemID.Sets.ShimmerTransformToItem[Type] = ModContent.ItemType<OldSeraphimHelmet>();
+    }
+
     public override void SetHealerDefaults() {
         Item.SetSizeValues(28, 26);
 
-        Item.SetShopValues(ItemRarityColor.White0, Item.sellPrice());
+        Item.SetShopValues(ItemRarityColor.Lime7, Item.sellPrice(gold: 5));
 
-        int defense = 0;
+        int defense = 16;
         Item.defense = defense;
     }
 
     public override void UpdateEquip(Player player) {
-        
+        ApplyEquipEffects(player);
+    }
+
+    public static void ApplyEquipEffects(Player player) {
+        player.GetDamage(DamageClass.Generic) -= 0.2f;
+        player.GetDamage(ThoriumDamageBase<HealerDamage>.Instance) += 0.35f;
+        player.manaCost -= 0.2f;
+        player.GetThoriumPlayer().healBonus += 2;
+        player.statManaMax2 += 60;
     }
 
     public override bool IsArmorSet(Item head, Item body, Item legs)
@@ -33,33 +48,58 @@ public sealed class SeraphimHelmet : ThoriumItem_HealerBase {
 
 [AutoloadEquip(EquipType.Body)]
 public sealed class SeraphimChestplate : ThoriumItem_HealerBase {
+    public override void SetStaticDefaults() {
+        Item.ResearchUnlockCount = 1;
+        ItemID.Sets.ShimmerTransformToItem[Type] = ModContent.ItemType<OldSeraphimChestplate>();
+    }
+
     public override void SetHealerDefaults() {
         Item.SetSizeValues(38, 26);
 
-        Item.SetShopValues(ItemRarityColor.White0, Item.sellPrice());
+        Item.SetShopValues(ItemRarityColor.Lime7, Item.sellPrice(gold: 6, silver: 40));
 
-        int defense = 0;
+        int defense = 22;
         Item.defense = defense;
     }
 
     public override void UpdateEquip(Player player) {
+        ApplyEquipEffects(player);
+    }
 
+    public static void ApplyEquipEffects(Player player) {
+        player.GetDamage(DamageClass.Generic) -= 0.3f;
+        player.GetDamage(ThoriumDamageBase<HealerDamage>.Instance) += 0.5f;
+        player.manaCost -= 0.2f;
+        player.GetThoriumPlayer().healBonus += 3;
     }
 }
 
 [AutoloadEquip(EquipType.Legs)]
 public sealed class SeraphimLegs : ThoriumItem_HealerBase {
+    public override void SetStaticDefaults() {
+        Item.ResearchUnlockCount = 1;
+        ItemID.Sets.ShimmerTransformToItem[Type] = ModContent.ItemType<OldSeraphimLegs>();
+    }
+
     public override void SetHealerDefaults() {
         Item.SetSizeValues(26, 20);
 
-        Item.SetShopValues(ItemRarityColor.White0, Item.sellPrice());
+        Item.SetShopValues(ItemRarityColor.Lime7, Item.sellPrice(gold: 4));
 
-        int defense = 0;
+        int defense = 18;
         Item.defense = defense;
     }
 
     public override void UpdateEquip(Player player) {
+        ApplyEquipEffects(player);
+    }
 
+    public static void ApplyEquipEffects(Player player) {
+        player.GetDamage(DamageClass.Generic) -= 0.2f;
+        player.GetDamage(ThoriumDamageBase<HealerDamage>.Instance) += 0.35f;
+        player.manaCost -= 0.15f;
+        player.GetThoriumPlayer().healBonus += 2;
+        player.moveSpeed += 0.15f;
     }
 }
 
